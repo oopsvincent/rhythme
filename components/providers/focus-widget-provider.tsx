a223fc9b9ea35394a9fc0795a84c352c42ae9ee5
oopsvincent <farhanalireza50@gmail.com>
2026-05-06 16:26:59 +0530

    Added cross device sync to focus sessions

--- components/providers/focus-widget-provider.tsx
+++ components/providers/focus-widget-provider.tsx
@@ -1,12 +1,15 @@
 'use client'
 
 import { FocusFloatingWidget } from '@/components/focus/focus-floating-widget'
+import { FocusSync } from '@/components/focus/focus-sync'
 
 export function FocusWidgetProvider({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
       <FocusFloatingWidget />
+      <FocusSync />
     </>
   )
 }
+
